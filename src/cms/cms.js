4c20a2101422892406e5f83f79706799d9f6a0e3
Rob MacKay <robskiwarrior@gmail.com>
2018-10-24 09:24:57 +0100

    Push

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,9 +1,11 @@
 import CMS from 'netlify-cms'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
-import BlogPostPreview from './preview-templates/BlogPostPreview'
+import ArticlesPreview from './preview-templates/ArticlesPreview'
+import DeveloperPreview from './preview-templates/DeveloperPreview'
 import ServicesPreview from './preview-templates/ServicesPreview'
 
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('services', ServicesPreview)
-CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.registerPreviewTemplate('articles', ArticlesPreview)
+CMS.registerPreviewTemplate('developer', DeveloperPreview)
