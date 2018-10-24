7827413c05f246365b267628e2f10b04f33535d6
Rob MacKay <robskiwarrior@gmail.com>
2018-10-24 09:42:06 +0100

    kill the developers

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,10 +2,8 @@ import CMS from 'netlify-cms'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import ArticlesPreview from './preview-templates/ArticlesPreview'
-import DeveloperPreview from './preview-templates/DeveloperPreview'
 import ServicesPreview from './preview-templates/ServicesPreview'
 
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('services', ServicesPreview)
 CMS.registerPreviewTemplate('articles', ArticlesPreview)
-CMS.registerPreviewTemplate('developer', DeveloperPreview)
