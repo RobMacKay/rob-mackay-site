d0508a7f47418046bcdf4bcfb40382b7da698198
Rob MacKay <robskiwarrior@gmail.com>
2018-10-24 09:02:18 +0100

    Lets see

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,8 +2,8 @@ import CMS from 'netlify-cms'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
+import ServicesPreview from './preview-templates/ServicesPreview'
 
 CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
+CMS.registerPreviewTemplate('services', ServicesPreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
